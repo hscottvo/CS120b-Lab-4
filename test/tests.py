100e2c562c05244ca0bd75e5428c210566ca1a30
hscottvo <hscottvo@gmail.com>
2021-04-18 00:55:38 -0700

    Added first tests for part 1

--- test/tests.py
+++ test/tests.py
@@ -14,16 +14,29 @@
 # An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
-tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
-    'expected': [('PORT',<val>)],
+tests = [ {'description': 'init',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 1 } ],
+    'expected': [('PORTB',0x01)],
     },
-    {'description': 'This test will run second.',
-    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
-        {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
-        {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
-        {'inputs': [('PIN',<val>)], 'time': 600}, ],
-    'expected': [('PORT',<val>)],
+    {'description': 'wait_1',
+    'steps': [ {'inputs': [('PINA', 0x00)],'iterations': 5},
+    'expected': [('PORTB',0x01)],
+    },
+    {'description': 'press_1',
+    'steps': [ {'inputs': [('PINA', 0x01)],'iterations': 5},
+    'expected': [('PORTB',0x02)],
+    },
+    {'description': 'off_1',
+    'steps': [ {'inputs': [('PINA', 0x00)],'iterations': 5},
+    'expected': [('PORTB',0x02)],
+    },
+    {'description': 'press_0',
+    'steps': [ {'inputs': [('PINA', 0x01)],'iterations': 5},
+    'expected': [('PORTB',0x02)],
+    },
+    {'description': 'off_0',
+    'steps': [ {'inputs': [('PINA', 0x00)],'iterations': 5},
+    'expected': [('PORTB',0x02)],
     },
     ]
 
