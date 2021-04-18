9162a8dd439f7e856b38b91e465a52ae9f0a9b1e
hscottvo <hscottvo@gmail.com>
2021-04-18 00:46:28 -0700

    Initial state machine to be tested

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: svo021
  *  Partner(s) Name: Scott Vo
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 4  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,10 +12,80 @@
 #include "simAVRHeader.h"
 #endif
 
+enum led_states {led_init, led_one, led_one_wait, led_zero, led_zero_wait} led_state;
+
+void led_tick() {
+    switch(led_state){
+        case led_init:
+            led_state = led_zero_wait;
+            break;
+        case led_one:
+            if (PINA & 0x01 == 1) {
+                led_state = led_one;
+            }
+            else {
+                led_state = led_one_wait;
+            }
+            break;
+        case led_one_wait:
+            if (PINA & 0x01 == 1) {
+                led_state = led_zero;
+            }
+            else {
+                led_state = led_one_wait;
+            }
+        case led_zero;
+            if (PINA & 0x01 == 1) {
+                led_state = led_zero;
+            }
+            else {
+                led_state = led_zero_wait;
+            }
+            break;
+        case led_zero_wait;
+            if (PINA & 0x01 == 1) {
+                led_state = led_one;
+            }
+            else {
+                led_state = led_zero_wait;
+            }
+            break;
+        default:
+            led_state = led_init;
+            break;
+
+    }
+
+    switch(led_state){
+        case led_init:
+            PORTB = 0x01;
+            break;
+        case led_one:
+            PORTB = 0x02;
+            break;
+        case led_one_wait:
+            PORTB = 0x02;
+            break;
+        case led_zero;
+            PORTB = 0x01;
+            break;
+        case led_zero_wait;
+            PORTB = 0x01;
+            break;
+        default:
+            PORTB = 0x01;
+            break;
+    }
+
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
 
     /* Insert your solution below */
+
     while (1) {
 
     }
